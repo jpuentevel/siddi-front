045e809ed7fd296f1fb1d61ef4ff1a63a5a58dda
jpuentevel <jpuentevelasquez@gmail.com>
2024-12-16 21:08:32 -0500

    little ajuste

--- components/views/view_aviso/ViewAviso.jsx
+++ components/views/view_aviso/ViewAviso.jsx
@@ -7,16 +7,13 @@ const ViewAviso = () => {
             <div className="flex min-h-screen flex-col items-center p-12 bg-purple-100">
                 <div className="flex flex-col items-center mb-5">
                     <h1 className="sm:text-5xl text-3xl text-center max-w-3xl mt-5 text-purple-700">
-                        <b>Aviso Importante</b>
+                        <b>Autorización de uso de datos</b>
                     </h1>
                     <p className="sm:leading-relaxed sm:text-3xl text-2xl text-justify max-w-3xl mt-10 text-gray-900">
-                        Este sistema de detección de desnutrición infantil ha sido desarrollado mediante el entrenamiento 
-                        de una red neuronal avanzada. ¿Le gustaría permitir que la imagen que analice hoy sea almacenada 
-                        para mejorar el sistema en el futuro? Su contribución ayudará a incrementar la precisión y 
-                        eficacia de nuestras evaluaciones.
+                        El Sistema de Detección de Desnutrición Infantil ha sido desarrollado mediante el entrenamiento de una red neuronal. ¿Autoriza usted que la imagen sea usada por el sistema y almacenada para mejorar su desempeño? Las imágenes almacenadas serán usadas exclusivamente para el funcionamiento del sistema.
                     </p>
                     <p className="sm:leading-relaxed sm:text-3xl text-2xl text-left max-w-3xl w-full mt-4 text-gray-900">
-                        Gracias por su apoyo.
+                        Agradecemos su apoyo.
                     </p>
                 </div>
                 <div className="sm:grid sm:grid-cols-2 sm:justify-items-center flex flex-col items-center mt-4 w-3xl">
@@ -32,7 +29,7 @@ const ViewAviso = () => {
                         type="button"
                         className="w-52 focus:outline-none text-purple-200 bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-center text-2xl px-3 py-2.5 mt-3 sm:ml-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
                     >
-                        Regresar
+                        Rechazar
                     </Link>
                 </div>
             </div>
